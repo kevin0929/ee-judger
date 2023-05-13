72636d5e784428863347506ee60461398abaa1d1
kevin0929 <tn781927@yahoo.com.tw>
2023-05-13 17:19:59 +0800

    [Revise] Modify execute logic (problem_num) and final csv maker.

--- dependencies.py
+++ dependencies.py
@@ -10,6 +10,7 @@ def check_folder_name(path: str) -> None:
     To get the name of every student's folder,
     and remove superfluous words except student id.
     """
+
     folder_path = os.path.join(path, "*")
     folder_list = glob.glob(folder_path)
 
@@ -35,42 +36,26 @@ def check_file_name(path: str) -> None:
     if file type is .cpp, change it to .c,
     and if file type is other, delete it
     """
+
     file_path = os.path.join(path, "*")
     file_list = glob.glob(file_path)
 
     # To set regex rule
-    file_name_regex = r"\d{10}_P\d.c"
+    file_name_regex = re.compile(r"\d{10}_P\d")
     for file in file_list:
         # Deal with path problem
-        file = file.replace("\\", "/")
-        file_name = file.split("/")[-1]
-        print(file_name)
-
-        # Check file type
-        file_type = file_name.split(".")[-1]
-        if file_type == "c":
-            continue
-        elif file_type == "cpp":
-            new_file_name = f"{file_name}.c"
-            os.rename(file, f"{path}\\{new_file_name}")
-        else:
-            os.remove(file)
-            continue
+        file_name = file.split("\\")[-1]
+        basename, typename = os.path.splitext(file_name)
 
         # Check P is upper
-        if file_name.find("p") != -1:
-            new_file_name = file_name.replace("p", "P")
-            os.rename(f"{path}/{file_name}", f"{path}/{new_file_name}")
+        basename = basename.replace("p", "P")
 
-        # Check .c instead .c.c
-        if file_name.find(".c.c") != -1:
-            new_file_name = f"{file_name}.c"
-            os.rename(f"{path}/{file_name}", f"{path}/{new_file_name}")
-
-        # Final check the file name is right
-        if not re.match(file_name_regex, file_name):
+        # Check file type
+        if typename == ".exe":
             os.remove(file)
-            logger.error(f"{file_name} format wrong!")
+        else:
+            new_file_name = file_name_regex.match(basename).group()
+            os.rename(file, f"{path}\\{new_file_name}.c")
 
 
 def check_all() -> None:
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import glob
 import os
+import re
 import io
 import subprocess
+import pandas as pd
 
 from config import logger
 from dependencies import check_all
+from tqdm import tqdm
 
 
 def compile(file_path: str) -> bool:
-    """ compile target program """
+    """compile target program"""
 
     compile_command = ["gcc", "-o", "test", str(file_path)]
     compile_result = subprocess.run(
@@ -22,29 +25,30 @@ def compile(file_path: str) -> bool:
         return False
 
 
-def execution(file_path: str, problem: int) -> int:
-    """ return target problem score """
+def execution(file_path: str, problem_number: int) -> int:
+    """return target problem score"""
 
     # First step : compile
     if not compile(file_path):
         file_name = file_path.split("\\")[-1]
-        logger.error(f"The {file_name} can not compile problem {problem}")
+        logger.error(f"The {file_name} can't compile problem {problem_number}")
         return 0
 
     this_problem_score = 0
     execution_command = ["./test"]
-    problem_path = f"test_data/P{problem}"
+    problem_path = f"test_data/P{problem_number}"
     input_list = glob.glob(os.path.join(f"{problem_path}/input", "*"))
     output_list = glob.glob(os.path.join(f"{problem_path}/output", "*"))
     # start judge
     length = len(input_list)
     for idx in range(length):
         # read input and except_output to run and dealwith output result
-        with open(input_list[idx], "r") as f_in, \
-             open(output_list[idx], "r") as f_out:
+        input = input_list[idx]
+        output = output_list[idx]
+        with open(input, "r") as f_in, open(output, "r") as f_out:
             input_data = f_in.read()
             except_output = f_out.read()
-        # print(f"input : {input_data}, output : {except_output}")
+
         execution_result = subprocess.run(
             execution_command,
             input=input_data.encode(),
@@ -63,19 +67,50 @@ def execution(file_path: str, problem: int) -> int:
     return this_problem_score
 
 
-def get_all_score(folder_path: str, problem_number: int) -> dict:
-    """ traverse all problem for one student """
+def get_all_score(folder_path: str, problem_total_number: int) -> dict:
+    """traverse all problem for one student"""
 
-    score_dict = {idx: None for idx in range(1, problem_number + 1)}
-    file_list = glob.glob(os.path.join(f"code/{folder_path}", "*"))
-    for idx in range(0, problem_number):
-        score = execution(file_list[idx], idx + 1)
-        score_dict[idx + 1] = score
+    score_dict = {idx: 0 for idx in range(1, problem_total_number + 1)}
+    file_list = glob.glob(os.path.join(folder_path, "*"))
+
+    for file in file_list:
+        # Fetch problem number
+        file_name = file.split("\\")[-1]
+        match = re.search(r"P(\d+)\.c", file_name)
+        problem_number = match.group(1)
+
+        try:
+            score = execution(file, problem_number)
+            score_dict[problem_number] = score
+        except Exception as err:
+            logger.error(f"{file_name} has someting wrong, error msg : {err}.")
+            score_dict[problem_number] = 0
 
     return score_dict
 
 
 if __name__ == "__main__":
     check_all()
-    score_dict = get_all_score("4111064130", 6)
-    print(score_dict)
+    score_df = pd.DataFrame(
+        columns=["P1", "P2", "P3", "P4", "P5", "P6"],
+        index=["0"],
+    )
+    student_list = glob.glob(os.path.join("code", "*"))
+
+    for student in tqdm(student_list):
+        student_id = student.split("\\")[-1]
+        print(student_id)
+        student_dict = get_all_score(student, 6)
+        new_df = pd.DataFrame(
+            {
+                "P1": student_dict[1],
+                "P2": student_dict[2],
+                "P3": student_dict[3],
+                "P4": student_dict[4],
+                "P5": student_dict[5],
+                "P6": student_dict[6],
+            },
+            index=[student_id],
+        )
+        score_df = pd.concat([score_df, new_df], axis=0)
+    score_df.to_csv("final_score.csv")
